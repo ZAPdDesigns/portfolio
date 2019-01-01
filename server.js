31c182f321e91d976b8f84fe0bd9b696860f9931
Kenny <kennygperez@gmail.com>
2018-12-31 22:50:34 -0500

    fixing router

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const server = express()
 
 server
     .disable('x-powered-by')
-    .get('/', (req, res) => res.status(200).sendFile(path.join(__dirname, 'dist', 'index.html')))
     .use('/js', express.static(path.join(__dirname, 'dist', 'js')))
     .use('/css', express.static(path.join(__dirname, 'dist', 'css')))
     .use('/fonts', express.static(path.join(__dirname, 'dist', 'fonts')))
     .use('/img', express.static(path.join(__dirname, 'dist', 'img')))
+    .get('/*', (req, res) => res.status(200).sendFile(path.join(__dirname, 'dist', 'index.html')))
     .listen(8082)
