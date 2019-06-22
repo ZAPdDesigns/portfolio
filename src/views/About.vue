1c0d82d082f87e785f8dffedc9fa6f26e7514ccb
Will Johnson <zapddesign@gmail.com>
2019-06-22 15:31:49 -0500

    Updated about page and favicon

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,7 +4,15 @@
 	<div class="about-img" :style="{'background-image': 'url(' + require('./../assets/me.jpg') + ')'}"></div>
 	<div class="about-text">
 		<h2>Hey, I'm Will.</h2>
-		<p>I'm from Central Kentucky, currently living in Nashville, Tennessee. I love working on various design projects, whether it be creating brands or designing sites and apps. I've been designing and playing with code since I was twelve. While I listen to a lot of music, I spend most of my time at my desk working on personal projects and other various projects.</p>
+		<p>
+			I'm from Central Kentucky, currently living in Nashville, Tennessee. I love working on various design projects, whether it be creating brands or designing sites and apps. I've been designing and playing with code since I was twelve. While I listen to a lot of music, I spend most of my time at my desk working on personal projects and other various projects.
+
+			<br>
+
+			I am from Central Kentucky, currently living in Nashville, Tennessee. I love working on various design projects, whether it be creating brands or designing sites and apps. I have been designing and playing with code since I was twelve. While I listen to a lot of music, I spend most of my time at my desk working on personal projects and other various projects.
+
+			I am currently a web designer at Visuals by Impulse, a remote-based design studio who has worked with thousands of streamers big and small. I create experiences for VBI's staff, front-facing clients, and anyone who uses VBI's products, whether it be the portal or the new Stream Design Factory app on the Streamlabs OBS app store.
+		</p>
 	</div>
 	</div>
 </div>
